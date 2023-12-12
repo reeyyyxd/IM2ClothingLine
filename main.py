05058f33e94d52dd9b9c235a54b13a808389fcee
Rey Anthony Novero <2woaranthony@gmail.com>
2023-12-12 10:09:55 +0800

    ready to rock and roll

---
+++ main.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_USER'] = "root"
+app.config['MYSQL_PASSWORD'] = "jasminrap"
+app.config['MYSQL_DB'] = "JJRREClothingLine"
+
+
+
+@app.route("/")
+def welcome():
+    return render_template('welcome.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/addCart")
+def addCart():
+    return render_template('addCart.html')
+
+@app.route("/checkout")
+def checkout():
+    return render_template('checkout.html')
+
+
+@app.route("/admin")
+def admin():
+    return render_template('admin.html')
+
+@app.route("/verification")
+def email_verification():
+
+    return render_template('verification.html')
+
+
+@app.route("/storage")
+def storage():
+    return render_template('storage.html')
+
+@app.route("/addStorage")
+def addStorage():
+    return render_template('addstorage.html')
+
+
+if __name__ =='__main__':
+    app.run(debug=True)
+
+
+
