1a456c949f3c601a1d5b84bfa6f18407095f0c5a
Rhyss <rhyssgianne123@gmail.com>
2023-12-12 14:27:02 +0800

    html

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
 app.config['MYSQL_PASSWORD'] = "jasminrap"
+# app.config['MYSQL_PASSWORD'] = "1234"
 app.config['MYSQL_DB'] = "JJRREClothingLine"
 
 
